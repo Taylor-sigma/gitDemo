86bfe3b2727ffe9c51a258bb4f804680bc099ca5
sigmoid_Wu <1139250021@qq.com>
2022-01-26 20:55:48 +0800

    你好

--- odesir.m
+++ odesir.m
@@ -1,4 +1,4 @@
-function dy = odesir(t, y, beta, gamma, N)
+function dy = odesir(t, y, beta, gamma, N)% the equation
 dy = [ -beta*y(1)*y(2)/N;                 
         beta*y(1)*y(2)/N - gamma*y(2);
                            gamma*y(2)];
